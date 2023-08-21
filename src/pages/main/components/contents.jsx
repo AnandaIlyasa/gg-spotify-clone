c46eec950ea770d0eea925616ddc636fcea001f4
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2023-08-21 23:28:15 +0700

    change iframe to audio tag for track preview

--- src/pages/main/components/contents.jsx
+++ src/pages/main/components/contents.jsx
@@ -5,24 +5,16 @@ export default function Contents({ contents }) {
         <div className="content-container">
             {contents?.items?.map(track => (
                 <li key={track.id} className="song-item">
-                    <div className="song-image">
-                        <img src={track.album.images[0]?.url} alt=""/>
-                        {
-                            track.preview_url ?
-                            <iframe 
-                                className="song-preview" 
-                                title={track.name} 
-                                src={track.preview_url}
-                                frameBorder="0" 
-                                allowtransparency="true"
-                                allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                                loading="lazy"
-                            ></iframe> :
-                            <Text className="song-preview" textAlign="center" paddingTop="15%" fontWeight="bold" backgroundColor="black">
-                                Preview Not Available
-                            </Text>
-                        }
-                    </div>
+                    <img src={track.album.images[0]?.url} alt=""/>
+                    {
+                        track.preview_url ?
+                        <audio className="song-preview" controls>
+                            <source src={track.preview_url}></source>
+                        </audio> :
+                        <Text className="song-preview" textAlign="center" fontWeight="bold" backgroundColor="black">
+                            Audio Not Available
+                        </Text>
+                    }
                     <Text noOfLines={1} fontWeight="bold">{track.name}</Text>
                     <Text noOfLines={1}>{track.artists?.map(artist => artist.name).join(", ")}</Text>
                 </li>
