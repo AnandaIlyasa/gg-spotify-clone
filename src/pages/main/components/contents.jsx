5a12b56f77c1e8f47dbf324d1524736f7a971d68
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2023-08-21 21:45:21 +0700

    adding preview

--- src/pages/main/components/contents.jsx
+++ src/pages/main/components/contents.jsx
@@ -5,7 +5,24 @@ export default function Contents({ contents }) {
         <div className="content-container">
             {contents?.items?.map(track => (
                 <li key={track.id} className="song-item">
-                    <img src={track.album.images[0]?.url} alt=""/>
+                    <div className="song-image">
+                        <img src={track.album.images[0]?.url} alt=""/>
+                        {
+                            track.preview_url ?
+                            <iframe 
+                                className="song-preview" 
+                                title={track.name} 
+                                src={track.preview_url}
+                                frameBorder="0" 
+                                allowtransparency="true"
+                                allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                                loading="lazy"
+                            ></iframe> :
+                            <Text className="song-preview" textAlign="center" paddingTop="15%" fontWeight="bold" backgroundColor="black">
+                                Preview Not Available
+                            </Text>
+                        }
+                    </div>
                     <Text noOfLines={1} fontWeight="bold">{track.name}</Text>
                     <Text noOfLines={1}>{track.artists?.map(artist => artist.name).join(", ")}</Text>
                 </li>
